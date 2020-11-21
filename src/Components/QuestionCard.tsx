d737daf7d84896a52154dfe8f6cb46d9ff196fcd
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-21 23:05:16 +0500

    update index css

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -1,46 +1,39 @@
-// import React, { useState } from 'react';
-// import { questionPropsType } from './../Types/quiz_types'
+import React, { useState } from 'react';
+import { questionPropsType } from './../Types/quiz_types'
 
-// const QuestionCard: React.FC<questionPropsType> = ({ question, options, callback }) => {
+const QuestionCard: React.FC<questionPropsType> = ({ question, options, callback }) => {
 
-//     let [selectedAns, setSelectedAns] = useState("");
+    let [selectedAns, setSelectedAns] = useState("");
 
-//     const handleSelection = (ev: any) => {
-//         setSelectedAns(ev.target.value);
-//     }
+    const handleSelection = (ev: any) => {
+        setSelectedAns(ev.target.value);
+    }
 
-//     return (
-//         <div className="question-container">
-//             <div className="question">
-//                 <h4>{question}</h4>
-//             </div>
+    return (
+        <div className="question-container">
+            <div className="question">
+                <h4>{question}</h4>
+            </div>
 
-//             <form onSubmit={(e: React.FormEvent<EventTarget>) => callback(e, selectedAns)}
-//                 className="question-form"
-//                 >
-//                 {
-//                     options.map((opt: string, ind: number) => {
-//                         return (
-//                             <div key={ind}>
-//                                 <label className="radio">
-//                                     <input
-//                                         type="radio"
-//                                         name="opt"
-//                                         required
-//                                         value={opt}
-//                                         checked={selectedAns === opt}
-//                                         onChange={handleSelection}
-//                                     />
-//                                     {opt}
-//                                 </label>
-//                             </div>
-//                         )
-//                     })
-//                 }
-//                 <input type="submit" className="submit"/>
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default QuestionCard;
+            <form onSubmit={(e: React.FormEvent<EventTarget>) => callback(e, selectedAns)}
+                className="question-form"
+                >
+                {
+                    options.map((opt: string, ind: number) => {
+                        return (
+                            <div key={ind}>
+                                <label className="radio">
+                                    <input
+                                        type="radio"
+                                        name="opt"
+                                        required
+                                        value={opt}
+                                        checked={selectedAns === opt}
+                                        onChange={handleSelection}
+                                    />
+                                    {opt}
+                                </label>
+                            </div>
+                        )
+                    })
+                }
